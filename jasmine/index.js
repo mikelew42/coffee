c133edf8391689054a40f3e781c89fe2aec14799
Michael <mike@lew42.com>
2016-07-28 02:56:04 -0500

    some stack trace stuff

--- jasmine/index.js
+++ jasmine/index.js
@@ -6,6 +6,25 @@ var sourceMappedStackTrace = require("../sourcemapped-stacktrace/sourcemapped-st
 
 jasmine.getEnv().addReporter({
 jasmineDone: function() {
+  var details = document.querySelectorAll(".jasmine-spec-detail");
+
+  details.forEach(function(detail){
+    var desc = detail.querySelector(".jasmine-description"),
+        messages = detail.querySelector(".jasmine-messages"),
+        msgs = messages.querySelectorAll(".jasmine-result-message");
+
+    console.groupCollapsed(desc.textContent);
+
+    msgs.forEach(function(msg){
+      var m = msg.textContent,
+          stack = msg.nextSibling.textContent;
+
+      console.log(stack);
+    });
+
+    console.groupEnd();
+  });
+
   var traces = document.querySelectorAll(".jasmine-stack-trace")
   // console.log(traces[0].textContent);
   for(var i = 0; i < traces.length; i++) {
@@ -14,7 +33,7 @@ jasmineDone: function() {
     	// console.log(sourceMappedStackTrace)
       sourceMappedStackTrace.mapStackTrace(node.textContent, function(stack) {
       	// console.log(stack);
-      	stack = stack.join("\n").replace(/\((webpack:\/\/\/.*)\)/gm, "(<a href='$1'>$1</a>)");
+      	stack = stack.join("\n") //.replace(/\((webpack:\/\/\/.*)\)/gm, "(<a href='$1'>$1</a>)");
         node.innerHTML = node.previousSibling.textContent + "\n" + stack
       })
     })(traces[i])
